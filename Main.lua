0fa38eab2db2cf421f1efd689ff0199abcfe02ce
yPedroX <pedrolucasa757@gmail.com>
2025-12-21 19:34:00 -0300

    update Main.lua

--- Main.lua
+++ Main.lua
@@ -14,13 +14,6 @@ local Window = WindUI:CreateWindow({
     Transparent = true,
 })
 
-local Window:Tag({
-    Title = "v1.6.6",
-    Icon = "github",
-    Color = Color3.fromHex("#30ff6a"),
-    Radius = 0, -- from 0 to 13
-})
-
 -- ==================== ABA PERFORMANCE ====================
 local PerformanceTab = Window:Tab({
     Title = "Principal",
